941881a40d75fdbdd51b904038cd027529a92f61
BrennanKnopf <brennanknopf@gmail.com>
2022-02-15 13:40:54 -0600

    Added more styling withing SongTable

--- music_library/src/Components/SearchBar/SearchBar.jsx
+++ music_library/src/Components/SearchBar/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Form from 'react-bootstrap/Form'
 
 const SearchBar = (props) => {
     
@@ -24,7 +25,7 @@ const SearchBar = (props) => {
     
     return (
         <form onSubmit={handleSubmit}>
-               <label>Search For Song, Artist, Album, Genre, or Release Date:</label>
+            <label>Search For Song, Artist, Album, Genre, or Release Date:</label>
                 <div>
                     <input title="search" type='text' name="searchTerm" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)}/>
                     <input type="button" value="Search" type="submit" className="btn btn-info" />
@@ -32,6 +33,12 @@ const SearchBar = (props) => {
                 </div>
             </form>
 
+            // <Form onSubmit={handleSubmit}>
+            // <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            //     <Form.Label>Search For Song, Artist, Album, Genre, or Release Date:</Form.Label>
+            // <Form.Control input="search" type='text' name="searchTerm" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
+            // </Form.Group>
+            // </Form>
      );
 }
  
--- music_library/src/Components/SongTable/SongTable.jsx
+++ music_library/src/Components/SongTable/SongTable.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
 
 const SongTable = (props) => {
 
@@ -8,35 +9,67 @@ const SongTable = (props) => {
     //     return <li>{song.title} </li>
     // })
     
-        return ( 
-            <div>
-                <h2>Song List</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Song title</th>
-                            <th>Album</th>
-                            <th>Artist</th>
-                            <th>Genre</th>
-                            <th>Release Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.filteredResults.map((song) => { //maps b
-                            return(
-                                <tr>
-                                <td>{song.title}</td>
-                                <td>{song.album}</td>
-                                <td>{song.artist}</td>
-                                <td>{song.genre}</td>
-                                <td>{song.releaseDate}</td>
-                                </tr>
-                            )}
-                            )}
-                    </tbody>
-                </table>
-            </div>
-         );
+        // return ( 
+        //     <div>
+        //         <h2>Song List</h2>
+        //         <table>
+        //             <thead>
+        //                 <tr>
+        //                     <th>Song title</th>
+        //                     <th>Album</th>
+        //                     <th>Artist</th>
+        //                     <th>Genre</th>
+        //                     <th>Release Date</th>
+        //                 </tr>
+        //             </thead>
+        //             <tbody>
+        //                 {props.filteredResults.map((song) => { //maps b
+        //                     return(
+        //                         <tr>
+        //                         <td>{song.title}</td>
+        //                         <td>{song.album}</td>
+        //                         <td>{song.artist}</td>
+        //                         <td>{song.genre}</td>
+        //                         <td>{song.releaseDate}</td>
+        //                         </tr>
+        //                     )}
+        //                     )}
+        //             </tbody>
+        //         </table>
+        //     </div>
+        //  );
+    
+    return(
+    <Table striped bordered hover variant="dark">
+        <thead>
+            <h1>Song List</h1>
+                <tr>
+                <th>Song title</th>
+                <th>Album</th>
+                <th>Artist</th>
+                <th>Genre</th>
+                <th>Release Date</th>
+                </tr>
+        </thead>
+        <tbody>
+        {props.filteredResults.map((song) => { //maps b
+            return(
+                <tr>
+                    <td>{song.title}</td>
+                     <td>{song.album}</td>
+                    <td>{song.artist}</td>
+                    <td>{song.genre}</td>
+                    <td>{song.releaseDate}</td>
+                </tr>
+            )}
+            )}
+        </tbody>
+    </Table>
+    )
     }
      
     export default SongTable;
+
+
+    
+
