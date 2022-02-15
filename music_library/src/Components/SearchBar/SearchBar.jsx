d2e1d5ecd7813818d4911e9bd58500703a332d85
BrennanKnopf <brennanknopf@gmail.com>
2022-02-15 13:52:18 -0600

    Added more styling to the input form.

--- music_library/src/Components/SearchBar/SearchBar.jsx
+++ music_library/src/Components/SearchBar/SearchBar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container'
 
 const SearchBar = (props) => {
     
@@ -24,21 +25,24 @@ const SearchBar = (props) => {
     
     
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Search For Song, Artist, Album, Genre, or Release Date:</label>
-                <div>
-                    <input title="search" type='text' name="searchTerm" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)}/>
-                    <input type="button" value="Search" type="submit" className="btn btn-info" />
-                    <button type="button" onClick={() => props.getAllSongs()} className="btn btn-info" >Reset</button>
-                </div>
-            </form>
-
-            // <Form onSubmit={handleSubmit}>
-            // <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            //     <Form.Label>Search For Song, Artist, Album, Genre, or Release Date:</Form.Label>
-            // <Form.Control input="search" type='text' name="searchTerm" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
-            // </Form.Group>
-            // </Form>
+        // <form onSubmit={handleSubmit}>
+        //     <label>Search For Song, Artist, Album, Genre, or Release Date:</label>
+        //         <div>
+        //             <input title="search" type='text' name="searchTerm" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)}/>
+        //             <input type="button" value="Search" type="submit" className="btn btn-info" />
+        //             <button type="button" onClick={() => props.getAllSongs()} className="btn btn-info" >Reset</button>
+        //         </div>
+        //     </form>
+        <Container>
+            <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+                <Form.Label>Search For Song, Artist, Album, Genre, or Release Date:</Form.Label>
+            <Form.Control input="search" type='text' name="searchTerm" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} size='lg' />
+            <input type="button" value="Search" type="submit" className="btn btn-info" />
+            <button type="button" onClick={() => props.getAllSongs()} className="btn btn-info" >Reset</button>  
+            </Form.Group>
+            </Form>
+        </Container>
      );
 }
  
